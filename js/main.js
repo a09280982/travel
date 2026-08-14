79b6198d2c903375abab8b911f2c539858f78683
a09280982 <a09280982@gmail.com>
2026-08-14 14:11:01 +0000

    新增主 JavaScript 檔案並在沖繩頁面中引入

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// ========================================
+// My Travel - Common JavaScript
+// ========================================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("My Travel website loaded!");
+
+});
